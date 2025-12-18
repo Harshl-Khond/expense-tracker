745c7c3c2238e33ef73390d20d86f182d00c5ae6
Harshl-khond <harshalkhond9876@gmail.com>
2025-12-18 12:02:44 +0530

    Use Firebase credentials from environment variable

--- backend/firebase_setup.py
+++ backend/firebase_setup.py
@@ -1,7 +1,13 @@
 import firebase_admin
-from firebase_admin import credentials, firestore,storage
+from firebase_admin import credentials, firestore
+import os, json
 
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+# Load Firebase credentials from environment variable
+firebase_creds = json.loads(os.environ.get("FIREBASE_CREDENTIALS"))
+
+cred = credentials.Certificate(firebase_creds)
+
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
 
 db = firestore.client()
