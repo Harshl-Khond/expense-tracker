2492451f2d35519cd7224297e584b5a250f34e33
Harshal Khond <155840455+Harshl-Khond@users.noreply.github.com>
2025-12-29 15:22:51 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -112,7 +112,7 @@ def add_expense():
         bill_image_base64 = data.get("bill_image")
         email = data.get("email")
 
-        if not all([date, description, amount, bill_image_base64, email]):
+        if not all([date, description, amount, email]):
             return jsonify({"error": "Missing fields"}), 400
 
         balance_doc = db.collection("fund_balance").document("main").get()
