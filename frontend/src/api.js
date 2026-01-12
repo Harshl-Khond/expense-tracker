26aa76335905c8564d22924fc64fdbdbe4a25cc5
Harshal Khond <155840455+Harshl-Khond@users.noreply.github.com>
2026-01-12 16:08:01 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,19 +1,46 @@
+// import axios from "axios";
+
+// export const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL,
+// });
+// // Automatically attach session_token to every request
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("session");
+
+//   if (token) {
+//     if (config.method === "get") {
+//       config.params = config.params || {};
+//       config.params.session_token = token;
+//     } else {
+//       config.data = config.data || {};
+//       config.data.session_token = token;
+//     }
+//   }
+
+//   return config;
+// });
+
 import axios from "axios";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+
 // Automatically attach session_token to every request
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("session");
+  const session = JSON.parse(localStorage.getItem("session"));
 
-  if (token) {
+  if (session?.session_token) {
     if (config.method === "get") {
-      config.params = config.params || {};
-      config.params.session_token = token;
+      config.params = {
+        ...config.params,
+        session_token: session.session_token,
+      };
     } else {
-      config.data = config.data || {};
-      config.data.session_token = token;
+      config.data = {
+        ...config.data,
+        session_token: session.session_token,
+      };
     }
   }
 
