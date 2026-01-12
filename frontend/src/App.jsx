0a6bcef1f7d4c5862e0af83ebb8d7710974da00e
Harshal Khond <155840455+Harshl-Khond@users.noreply.github.com>
2026-01-12 15:31:58 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
-import Signup from "./pages/Signup";
+import Signup from "./pages/signup";
 import Login from "./pages/Login";
 
 import EmployeeDashboard from "./pages/EmployeeDashboard";
